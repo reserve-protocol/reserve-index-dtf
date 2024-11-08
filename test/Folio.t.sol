2d352e8de64a733b5ed0a25b1a411c8493bc7d01
Patrick McKelvy <pmckelvy1@gmail.com>
2024-11-08 16:17:28 -0500

    test dao fee registry.

--- test/Folio.t.sol
+++ test/Folio.t.sol
@@ -15,8 +15,9 @@ contract FolioTest is BaseTest {
         amounts[0] = D6_TOKEN_10K;
         amounts[1] = D18_TOKEN_10K;
         amounts[2] = D27_TOKEN_10K;
-        IFolio.DemurrageRecipient[] memory recipients = new IFolio.DemurrageRecipient[](1);
-        recipients[0] = IFolio.DemurrageRecipient(owner, 10000);
+        IFolio.DemurrageRecipient[] memory recipients = new IFolio.DemurrageRecipient[](2);
+        recipients[0] = IFolio.DemurrageRecipient(owner, 9000);
+        recipients[1] = IFolio.DemurrageRecipient(feeReceiver, 1000);
         // 1% demurrage fee
         vm.startPrank(owner);
         USDC.approve(address(folioFactory), type(uint256).max);
@@ -30,27 +31,30 @@ contract FolioTest is BaseTest {
 
     function test_deployment() public {
         _deployTestFolio();
-        assertEq(folio.name(), "Test Folio");
-        assertEq(folio.symbol(), "TFOLIO");
-        assertEq(folio.decimals(), 18);
-        assertEq(folio.totalSupply(), 1e18 * 10000);
-        assertEq(folio.balanceOf(owner), 1e18 * 10000);
-        assertEq(folio.assets().length, 3);
-        assertEq(folio.assets()[0], address(USDC));
-        assertEq(folio.assets()[1], address(DAI));
-        assertEq(folio.assets()[2], address(MEME));
-        assertEq(USDC.balanceOf(address(folio)), D6_TOKEN_10K);
-        assertEq(DAI.balanceOf(address(folio)), D18_TOKEN_10K);
-        assertEq(MEME.balanceOf(address(folio)), D27_TOKEN_10K);
-        assertEq(folio.demurrageFee(), 100);
-        (address recipient, uint256 bps) = folio.demurrageRecipients(0);
-        assertEq(recipient, owner);
-        assertEq(bps, 10000);
+        assertEq(folio.name(), "Test Folio", "wrong name");
+        assertEq(folio.symbol(), "TFOLIO", "wrong symbol");
+        assertEq(folio.decimals(), 18, "wrong decimals");
+        assertEq(folio.totalSupply(), 1e18 * 10000, "wrong total supply");
+        assertEq(folio.balanceOf(owner), 1e18 * 10000, "wrong owner balance");
+        assertEq(folio.assets().length, 3, "wrong assets length");
+        assertEq(folio.assets()[0], address(USDC), "wrong first asset");
+        assertEq(folio.assets()[1], address(DAI), "wrong second asset");
+        assertEq(folio.assets()[2], address(MEME), "wrong third asset");
+        assertEq(USDC.balanceOf(address(folio)), D6_TOKEN_10K, "wrong folio usdc balance");
+        assertEq(DAI.balanceOf(address(folio)), D18_TOKEN_10K, "wrong folio dai balance");
+        assertEq(MEME.balanceOf(address(folio)), D27_TOKEN_10K, "wrong folio meme balance");
+        assertEq(folio.demurrageFee(), 100, "wrong demurrage fee");
+        (address r1, uint256 bps1) = folio.demurrageRecipients(0);
+        assertEq(r1, owner, "wrong first recipient");
+        assertEq(bps1, 9000, "wrong first recipient bps");
+        (address r2, uint256 bps2) = folio.demurrageRecipients(1);
+        assertEq(r2, feeReceiver, "wrong second recipient");
+        assertEq(bps2, 1000, "wrong second recipient bps");
     }
 
     function test_mint() public {
         _deployTestFolio();
-        assertEq(folio.balanceOf(user1), 0);
+        assertEq(folio.balanceOf(user1), 0, "wrong starting user1 balance");
         uint256 startingUSDCBalance = USDC.balanceOf(address(folio));
         uint256 startingDAIBalance = DAI.balanceOf(address(folio));
         uint256 startingMEMEBalance = MEME.balanceOf(address(folio));
@@ -59,15 +63,30 @@ contract FolioTest is BaseTest {
         DAI.approve(address(folio), type(uint256).max);
         MEME.approve(address(folio), type(uint256).max);
         folio.mint(1e22, user1);
-        assertEq(folio.balanceOf(user1), 1e22);
-        assertApproxEqAbs(USDC.balanceOf(address(folio)), startingUSDCBalance + D6_TOKEN_10K, 1);
-        assertApproxEqAbs(DAI.balanceOf(address(folio)), startingDAIBalance + D18_TOKEN_10K, 1);
-        assertApproxEqAbs(MEME.balanceOf(address(folio)), startingMEMEBalance + D27_TOKEN_10K, 1e9);
+        assertEq(folio.balanceOf(user1), 1e22, "wrong user1 balance");
+        assertApproxEqAbs(
+            USDC.balanceOf(address(folio)),
+            startingUSDCBalance + D6_TOKEN_10K,
+            1,
+            "wrong folio usdc balance"
+        );
+        assertApproxEqAbs(
+            DAI.balanceOf(address(folio)),
+            startingDAIBalance + D18_TOKEN_10K,
+            1,
+            "wrong folio dai balance"
+        );
+        assertApproxEqAbs(
+            MEME.balanceOf(address(folio)),
+            startingMEMEBalance + D27_TOKEN_10K,
+            1e9,
+            "wrong folio meme balance"
+        );
     }
 
     function test_redeem() public {
         _deployTestFolio();
-        assertEq(folio.balanceOf(user1), 0);
+        assertEq(folio.balanceOf(user1), 0, "wrong starting user1 balance");
         vm.startPrank(user1);
         USDC.approve(address(folio), type(uint256).max);
         DAI.approve(address(folio), type(uint256).max);
@@ -81,12 +100,42 @@ contract FolioTest is BaseTest {
         uint256 startingDAIBalanceAlice = DAI.balanceOf(address(user1));
         uint256 startingMEMEBalanceAlice = MEME.balanceOf(address(user1));
         folio.redeem(5e21, user1, user1);
-        assertApproxEqAbs(USDC.balanceOf(address(folio)), startingUSDCBalanceFolio - D6_TOKEN_10K / 2, 1);
-        assertApproxEqAbs(DAI.balanceOf(address(folio)), startingDAIBalanceFolio - D18_TOKEN_10K / 2, 1);
-        assertApproxEqAbs(MEME.balanceOf(address(folio)), startingMEMEBalanceFolio - D27_TOKEN_10K / 2, 1e9);
-        assertApproxEqAbs(USDC.balanceOf(user1), startingUSDCBalanceAlice + D6_TOKEN_10K / 2, 1);
-        assertApproxEqAbs(DAI.balanceOf(user1), startingDAIBalanceAlice + D18_TOKEN_10K / 2, 1);
-        assertApproxEqAbs(MEME.balanceOf(user1), startingMEMEBalanceAlice + D27_TOKEN_10K / 2, 1e9);
+        assertApproxEqAbs(
+            USDC.balanceOf(address(folio)),
+            startingUSDCBalanceFolio - D6_TOKEN_10K / 2,
+            1,
+            "wrong folio usdc balance"
+        );
+        assertApproxEqAbs(
+            DAI.balanceOf(address(folio)),
+            startingDAIBalanceFolio - D18_TOKEN_10K / 2,
+            1,
+            "wrong folio dai balance"
+        );
+        assertApproxEqAbs(
+            MEME.balanceOf(address(folio)),
+            startingMEMEBalanceFolio - D27_TOKEN_10K / 2,
+            1e9,
+            "wrong folio meme balance"
+        );
+        assertApproxEqAbs(
+            USDC.balanceOf(user1),
+            startingUSDCBalanceAlice + D6_TOKEN_10K / 2,
+            1,
+            "wrong alice usdc balance"
+        );
+        assertApproxEqAbs(
+            DAI.balanceOf(user1),
+            startingDAIBalanceAlice + D18_TOKEN_10K / 2,
+            1,
+            "wrong alice dai balance"
+        );
+        assertApproxEqAbs(
+            MEME.balanceOf(user1),
+            startingMEMEBalanceAlice + D27_TOKEN_10K / 2,
+            1e9,
+            "wrong alice meme balance"
+        );
     }
 
     function test_daoFee() public {
@@ -97,10 +146,17 @@ contract FolioTest is BaseTest {
         uint256 demFeeBps = folio.demurrageFee();
         uint256 expectedFeeShares = (currentSupply * demFeeBps) / 1e4 / 2;
         uint256 pendingFeeShares = folio.getPendingFeeShares();
-        assertEq(expectedFeeShares, pendingFeeShares);
+        assertEq(expectedFeeShares, pendingFeeShares, "wrong pending fee shares");
+
+        uint256 initialOwnerShares = folio.balanceOf(owner);
         folio.distributeFees();
+
         (, uint256 daoFeeNumerator, uint256 daoFeeDenominator) = daoFeeRegistry.getFeeDetails(address(folio));
         uint256 expectedDaoShares = (pendingFeeShares * daoFeeNumerator) / daoFeeDenominator;
-        assertEq(folio.balanceOf(address(dao)), expectedDaoShares);
+        assertEq(folio.balanceOf(address(dao)), expectedDaoShares, "wrong dao shares");
+
+        uint256 remainingShares = pendingFeeShares - expectedDaoShares;
+        assertEq(folio.balanceOf(owner), initialOwnerShares + (remainingShares * 9000) / 10000, "wrong owner shares");
+        assertEq(folio.balanceOf(feeReceiver), (remainingShares * 1000) / 10000, "wrong fee receiver shares");
     }
 }
