9c975b87710a442438c658f49890e7ee02238287
Julian R <julianmrodri@gmail.com>
2024-11-27 10:56:49 -0300

    update solidity version

--- test/Governance.t.sol
+++ test/Governance.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.25;
+pragma solidity 0.8.28;
 
 import "./base/BaseTest.sol";
 import { Governance } from "contracts/Governance.sol";
--- test/utils/MockERC20Votes.sol
+++ test/utils/MockERC20Votes.sol
@@ -1,5 +1,5 @@
 //  SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
