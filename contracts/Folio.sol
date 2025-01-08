7dbd06629d65c8d207399d80dda7ac52a9816cdf
Taylor Brent <taylor.w.brent@gmail.com>
2025-01-08 12:36:10 -0500

    [TRST-R-2] make all Folio setters nonReentrant (#45)
    Co-authored-by: Akshat Mittal <itsreallyakshat@gmail.com>

--- contracts/Folio.sol
+++ contracts/Folio.sol
@@ -143,6 +143,7 @@ contract Folio is
 
     // ==== Governance ====
 
+
     function addToBasket(IERC20 token) external onlyRole(DEFAULT_ADMIN_ROLE) {
         require(basket.add(address(token)), Folio__BasketModificationFailed());
 
@@ -155,6 +156,7 @@ contract Folio is
         emit BasketTokenRemoved(address(token));
     }
 
+    /// @dev Non-reentrant via distributeFees()
     /// @param _newFee D18{1/s} Fee per second on AUM
     function setFolioFee(uint256 _newFee) external onlyRole(DEFAULT_ADMIN_ROLE) {
         distributeFees();
@@ -162,6 +164,7 @@ contract Folio is
         _setFolioFee(_newFee);
     }
 
+    /// @dev Non-reentrant via distributeFees()
     /// @param _newFee D18{1} Fee on mint
     function setMintingFee(uint256 _newFee) external onlyRole(DEFAULT_ADMIN_ROLE) {
         distributeFees();
@@ -169,7 +172,8 @@ contract Folio is
         _setMintingFee(_newFee);
     }
 
-    /// _newRecipients.portion must sum to 1e18
+    /// @dev Non-reentrant via distributeFees()
+    /// @param _newRecipients.portion must sum to 1e18
     /// @dev Fee recipients must be unique and sorted by address
     function setFeeRecipients(FeeRecipient[] memory _newRecipients) external onlyRole(DEFAULT_ADMIN_ROLE) {
         distributeFees();
@@ -178,12 +182,12 @@ contract Folio is
     }
 
     /// @param _newDelay {s} Delay after a trade has been approved before it can be permissionlessly opened
-    function setTradeDelay(uint256 _newDelay) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setTradeDelay(uint256 _newDelay) external nonReentrant onlyRole(DEFAULT_ADMIN_ROLE) {
         _setTradeDelay(_newDelay);
     }
 
     /// @param _newLength {s} Length of an auction
-    function setAuctionLength(uint256 _newLength) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function setAuctionLength(uint256 _newLength) external nonReentrant onlyRole(DEFAULT_ADMIN_ROLE) {
         _setAuctionLength(_newLength);
     }
 
