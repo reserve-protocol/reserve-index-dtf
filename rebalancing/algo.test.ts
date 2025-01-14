d6cc2e7ff4264a48a4de64cf65e77dc9e0049cab
Taylor Brent <taylor.w.brent@gmail.com>
2025-01-14 18:05:12 -0500

    swapout testing framework

--- rebalancing/algo.test.ts
+++ rebalancing/algo.test.ts
@@ -1,22 +1,56 @@
-import test from "ava";
+import { describe, it } from "node:test";
+import expect from "expect";
 
-import { getRebalance } from "./algo";
+import { getRebalance, Trade } from "./algo";
 
-const shares = BigInt(1e27);
+const ZERO = BigInt(0);
 
-test("[100%, 0%, 0%] => [0%, 50%, 50%]", (t) => {
-  const tokens = ["USDC", "DAI", "USDT"];
-  const bals = [BigInt(1e21), BigInt(0), BigInt(0)];
-  const targetBasket = [BigInt(0), BigInt(5e17), BigInt(5e17)];
-  const prices = [BigInt(1e6), BigInt(1e18), BigInt(1e6)];
-  const error = [BigInt(1e16), BigInt(1e16), BigInt(1e16)];
+const bn = (base: number, exp: number): bigint => {
+  return BigInt(base.toString() + "0".repeat(exp));
+};
 
-  const trades = getRebalance(shares, tokens, bals, targetBasket, prices, error);
-  t.is(trades.length, 2);
-  t.is(trades[0].sell, "USDC");
-  t.is(trades[0].buy, "DAI");
-  t.is(trades[1].sell, "USDC");
-  t.is(trades[1].buy, "USDT");
+const D18: bigint = bn(1, 18);
 
-  // TODO
+const shares: bigint = bn(1, 27);
+
+const assertApproxEq = (a: bigint, b: bigint, precision: bigint) => {
+  console.log("a", a);
+  console.log("b", b);
+  console.log("precision", precision);
+  const delta = a > b ? a - b : b - a;
+  expect(delta).toBeLessThanOrEqual((precision * a) / D18);
+};
+
+const expectTradeApprox = (
+  trade: Trade,
+  sell: string,
+  buy: string,
+  sellLimit: bigint,
+  buyLimit: bigint,
+  startPrice: bigint,
+  endPrice: bigint,
+  precision: bigint = BigInt(1e15), // 0.1%
+) => {
+  expect(trade.sell).toBe(sell);
+  expect(trade.buy).toBe(buy);
+
+  assertApproxEq(trade.sellLimit, sellLimit, precision);
+  assertApproxEq(trade.buyLimit, buyLimit, precision);
+  assertApproxEq(trade.startPrice, startPrice, precision);
+  assertApproxEq(trade.endPrice, endPrice, precision);
+};
+
+describe("getRebalance()", () => {
+  it("[100%, 0%, 0%] => [0%, 50%, 50%]", () => {
+    const tokens = ["USDC", "DAI", "USDT"];
+    const bals = [bn(1, 21), ZERO, ZERO];
+    const targetBasket = [ZERO, bn(5, 17), bn(5, 17)];
+    const prices = [bn(1, 6), bn(1, 18), bn(1, 6)];
+    const error = [bn(1, 16), bn(1, 16), bn(1, 16)];
+
+    const trades = getRebalance(shares, tokens, bals, targetBasket, prices, error);
+    expect(trades.length).toBe(2);
+    expectTradeApprox(trades[0], "USDC", "DAI", ZERO, bn(5, 53), bn(101, 37), bn(99, 37));
+    expectTradeApprox(trades[1], "USDC", "USDT", ZERO, bn(5, 65), bn(101, 25), bn(99, 25));
+  });
 });
