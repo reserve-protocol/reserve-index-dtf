2ad037c1e1cc29e87149c13eec5a9aa01c79fb14
Taylor Brent <taylor.w.brent@gmail.com>
2025-01-30 16:30:14 -0500

    close auction on reaching sellLimit (#91)

--- contracts/Folio.sol
+++ contracts/Folio.sol
@@ -561,11 +561,15 @@ contract Folio is
 
         emit AuctionBid(auctionId, sellAmount, boughtAmt);
 
-        // QoL feature: close auction and eject token from basket if we have sold all of it
-        if (auction.sell.balanceOf(address(this)) == 0) {
-            _removeFromBasket(address(auction.sell));
-
+        // QoL: close auction if we have reached the sell limit
+        sellBal = auction.sell.balanceOf(address(this));
+        if (sellBal <= minSellBal) {
             auction.end = block.timestamp;
+            // cannot update sellEnds/buyEnds due to possibility of parallel auctions
+
+            if (sellBal == 0) {
+                _removeFromBasket(address(auction.sell));
+            }
         }
 
         // collect payment from bidder
