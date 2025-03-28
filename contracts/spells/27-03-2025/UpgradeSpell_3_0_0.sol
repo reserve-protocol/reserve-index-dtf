899b4b79af1a4417d4b7f938a12fbeaf4b0972b2
Taylor Brent <taylor.w.brent@gmail.com>
2025-03-27 21:56:02 -0400

    naming + add Folio upgrade

--- contracts/spells/27-03-2025/UpgradeSpell_3_0_0.sol
+++ contracts/spells/27-03-2025/UpgradeSpell_3_0_0.sol
@@ -6,6 +6,8 @@ import { TimelockController } from "@openzeppelin/contracts/governance/TimelockC
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 import { IGovernanceDeployer } from "@interfaces/IGovernanceDeployer.sol";
+import { FolioDeployer } from "@deployer/FolioDeployer.sol";
+import { FolioProxyAdmin } from "@folio/FolioProxy.sol";
 import { FolioGovernor } from "@gov/FolioGovernor.sol";
 import { Folio } from "@src/Folio.sol";
 import { Versioned } from "@utils/Versioned.sol";
@@ -15,14 +17,29 @@ import { Versioned } from "@utils/Versioned.sol";
  * @author akshatmittal, julianmrodri, pmckelvy1, tbrent
  *
  * The 1.0.0 governors were all deployed with 100x the proposal threshold due to a frontend bug. This spell enables
- * easy upgrade and replacement of these governors.
+ * easy upgrade of the Folios and replacement of the governors/timelocks.
+ *
+ * It should work for upgrading Folios to EITHER 2.0.0 and 3.0.0.
  *
  * See dev comments below for details on how to use each function.
  */
-contract GovernanceUpgrader is Versioned {
+contract UpgradeSpell_3_0_0 is Versioned {
+    bytes32 constant VERSION_2_0_0 = keccak256("2.0.0");
+    bytes32 constant VERSION_3_0_0 = keccak256("3.0.0");
+
+    bytes32 public immutable folioVersionHash;
+
+    FolioDeployer public immutable folioDeployer;
     IGovernanceDeployer public immutable governanceDeployer;
 
-    constructor(IGovernanceDeployer _governanceDeployer) {
+    constructor(FolioDeployer _folioDeployer, IGovernanceDeployer _governanceDeployer) {
+        folioVersionHash = keccak256(bytes(_folioDeployer.version()));
+        bytes32 govVersion = keccak256(bytes(Versioned(address(_governanceDeployer)).version()));
+
+        require(folioVersionHash == VERSION_2_0_0 || folioVersionHash == VERSION_3_0_0, "invalid folio version");
+        require(govVersion == VERSION_2_0_0 || govVersion == VERSION_3_0_0, "invalid governance version");
+
+        folioDeployer = _folioDeployer;
         governanceDeployer = _governanceDeployer;
     }
 
@@ -46,9 +63,9 @@ contract GovernanceUpgrader is Versioned {
     /// @dev Expected use: governance atomically grants DEFAULT_ADMIN_ROLE to this contract AND
     ///                    transfers ownership of the proxy admin to this contract, pre-call.
     /// @dev Do not leave space after granting adminships for others to interact with this contract!
-    function upgradeFolioGovernance(
+    function upgradeFolioPlusGovernance(
         Folio folio,
-        Ownable proxyAdmin,
+        FolioProxyAdmin proxyAdmin,
         FolioGovernor oldOwnerGovernor,
         FolioGovernor oldTradingGovernor,
         address[] calldata guardians,
@@ -69,6 +86,19 @@ contract GovernanceUpgrader is Versioned {
         require(folio.hasRole(folio.DEFAULT_ADMIN_ROLE(), address(this)), "not admin");
         require(folio.hasRole(folio.DEFAULT_ADMIN_ROLE(), oldOwnerTimelock), "old owner timelock not admin");
         require(folio.hasRole(folio.AUCTION_APPROVER(), oldTradingTimelock), "old trading timelock not trader");
+
+        // upgrade Folio
+
+        if (folioVersionHash == VERSION_2_0_0) {
+            proxyAdmin.upgradeToVersion(address(folio), VERSION_2_0_0, "");
+        } else {
+            bytes memory data = abi.encodeWithSelector(
+                Folio.setTrustedFillerRegistry.selector,
+                folioDeployer.trustedFillerRegistry(),
+                true
+            );
+            proxyAdmin.upgradeToVersion(address(folio), folioVersionHash, data);
+        }
 
         // deploy replacement governors + timelocks
 
@@ -104,8 +134,6 @@ contract GovernanceUpgrader is Versioned {
         assert(folio.hasRole(folio.AUCTION_APPROVER(), newTradingTimelock));
         assert(folio.getRoleMemberCount(folio.DEFAULT_ADMIN_ROLE()) == 1);
         assert(folio.getRoleMemberCount(folio.AUCTION_APPROVER()) == 1);
-
-        // TODO decide if we want to upgrade the folio too
     }
 
     // ==== Internal ====
@@ -139,7 +167,7 @@ contract GovernanceUpgrader is Versioned {
         require(proposalThreshold >= 1e14 && proposalThreshold <= 1e17, "proposal threshold not in expected range");
 
         uint256 quorumThreshold = oldGovernor.quorumNumerator() * 1e16; // multiply by 1e16 to convert raw percent to D18{1}
-        require(quorumThreshold != 0 && quorumThreshold <= 1e17, "quorum threshold not in expected range");
+        require(quorumThreshold != 0 && quorumThreshold <= 2e17, "quorum threshold not in expected range");
 
         // stack-too-deep
         uint256 timelockDelay;
