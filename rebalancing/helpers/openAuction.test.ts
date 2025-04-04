e213a984372d24e78d302a3c26b32de2fa2b57a2
Taylor Brent <taylor.w.brent@gmail.com>
2025-04-04 15:55:18 -0400

    add optional ejectFully arg to openAuction()

--- rebalancing/helpers/openAuction.test.ts
+++ rebalancing/helpers/openAuction.test.ts
@@ -35,11 +35,25 @@ describe("openAuction()", () => {
     const prices = [1, 1];
     const error = [0.01, 0.01];
 
-    const result = openAuction(auction, supply, tokens, decimals, targetBasket, prices, error, 1);
-    // should use buyLimit.high
+    const result = openAuction(auction, supply, tokens, decimals, targetBasket, prices, error, 1, true);
+    // should use high buyLimit.high
     const expectedResult = [bn("0"), bn("1e27") + bn("1e25"), bn("1.01e39"), bn("0.99e39")];
     checkResult(result, expectedResult);
     expect(result[1]).toBeGreaterThan(bn("1e27"));
+  });
+
+  it("target: [0%, 100%] -- variant 2", () => {
+    const auction = makeAuction("USDC", "DAI", bn("0"), bn("1e27"), bn("1.02e39"), bn("0.98e39"), bn("1e16"));
+    const tokens = ["USDC", "DAI"];
+    const decimals = [bn("6"), bn("18")];
+    const targetBasket = [bn("0"), bn("1e18")];
+    const prices = [1, 1];
+    const error = [0.01, 0.01];
+
+    const result = openAuction(auction, supply, tokens, decimals, targetBasket, prices, error, 1, false);
+    // should not use buyLimit.high
+    const expectedResult = [bn("0"), bn("1e27"), bn("1.01e39"), bn("0.99e39")];
+    checkResult(result, expectedResult);
   });
 
   it("target: [50%, 50%]", () => {
