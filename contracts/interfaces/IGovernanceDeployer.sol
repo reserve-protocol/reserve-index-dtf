6f0ddae5f6f2be504fe04771e2deb3aa2ded0d71
Taylor Brent <taylor.w.brent@gmail.com>
2025-11-01 11:10:55 -0400

    draft vlDTF

--- contracts/interfaces/IGovernanceDeployer.sol
+++ contracts/interfaces/IGovernanceDeployer.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { IVotes } from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import { StakingVault } from "@staking/StakingVault.sol";
 
 interface IGovernanceDeployer {
     struct GovParams {
@@ -14,6 +16,14 @@ interface IGovernanceDeployer {
         // Roles
         address[] guardians; // Canceller Role
     }
+
+    function deployGovernedStakingToken(
+        string memory name,
+        string memory symbol,
+        IERC20 underlying,
+        IGovernanceDeployer.GovParams calldata govParams,
+        bytes32 deploymentNonce
+    ) external returns (StakingVault stToken, address governor, address timelock);
 
     function deployGovernanceWithTimelock(
         IGovernanceDeployer.GovParams calldata govParams,
